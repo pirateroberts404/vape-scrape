3025e0efa2954f8423ad45b1a5d578e980684981
mateoibarguen <mibargue@calpoly.edu>
2019-03-19 17:39:31 -0700

    Fixed licenses not joined.

--- interface/user_input.py
+++ interface/user_input.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import tkinter as tk
 from tkinter import ttk
 from tkinter import *
@@ -69,9 +70,6 @@ class Licenses:
       self.licenses_df['License Type'] = self.licenses_df['License Type'].fillna('').str.upper()
       self.licenses_df['License Number'] = self.licenses_df['License Number'].str.replace("-", "")
       
-   def removeSymbols(self, license_number):
-      return re.sub('[^0-9]','', license_number)
-   
 
    def getLicenseTuple(self, license_number):
       try:
@@ -83,69 +81,90 @@ class Licenses:
          print ("Could find license number {} ".format(license_number))
 
 
-   
-class JoinedFile(Licenses, WeedMapStores):
-   def __init__(self, licenses_location, wm_location, joined_location):
+class Joining(Licenses, WeedMapStores):
+   def __init__(self, licenses_location, wm_location, joined_json_location):
       Licenses.__init__(self, licenses_location)
       WeedMapStores.__init__(self, wm_location)
-      self.joined_location = joined_location
-      self.joined_dict = self.get_joined_dictionary(self.joined_location)
+      self.joined_json_location = joined_json_location
+      self.joined_dict = self.get_joined_dictionary(self.joined_json_location)
       self.top_ids = self.get_top_ids()
 
-   def get_joined_dictionary(self, joined_location):
-      with open (joined_location) as joined_file:
+
+   def get_joined_dictionary(self, joined_json_location):
+      with open (joined_json_location) as joined_file:
          joined_data = json.load(joined_file)
 
       return joined_data
 
    def traverse_list(self, current_license_number, forward = True):
       current_license_idx = self.top_ids.index(current_license_number)
+   
       if forward:
+         if current_license_idx >= len(self.top_ids) - 1:
+            return -1
+         if len(self.joined_dict[current_license_number]) < 1:
+            self.add_join(current_license_number, -1)
          return self.top_ids[current_license_idx + 1]
-      return self.top_ids[current_license_idx - 1]
 
-   def update_list(self, current_license_number):
-      joined_updated = self.get_joined_dictionary(self.joined_location)
-      return joined_updated[current_license_number]
+
+      if -1 in self.joined_dict[current_license_number]:
+         self.delete_join(current_license_number, -1)
+      return self.top_ids[current_license_idx - 1]
 
    def get_top_ids(self):
       return list(self.joined_dict.keys())
 
    def get_last_filled(self):
-      empty_item = next((k, v) for k, v in self.joined_dict.items() if len(v) < 1)
+      joined_list = [(k,v) for k,v in self.joined_dict.items()]
+      reversed_joined_list = reversed(joined_list)
+      empty_item = next((k, v) for k, v in reversed_joined_list if len(v) >= 1)
       index_empty = self.top_ids.index(empty_item[0])
-      return self.top_ids[index_empty - 1]
+      return self.top_ids[index_empty]
+
+   def invert_dict(self, d):
+      return dict([(v, k) for k, v in iter(d.items())])
 
    def add_join(self, license_id, wm_id):
-      with open(self.joined_location) as joined_file:
+      with open(self.joined_json_location) as joined_file:
          joined_data = json.load(joined_file)
 
       wm_ids_at_key = joined_data[license_id]
       if int(wm_id) not in wm_ids_at_key:
-         print ("not in wm_ids_at_key")
          wm_ids_at_key.append(int(wm_id))
          joined_data[license_id] = wm_ids_at_key
+         self.joined_dict = joined_data
 
-         with open(self.joined_location, 'w') as joined_file:
+         with open(self.joined_json_location, 'w') as joined_file:
             json.dump(joined_data, joined_file)
 
+   def add_join_file(self, license_number, wm_id):
+      complete_licenses = pd.read_csv("")
+      if wm_id != -1:
+         store_found = stores[stores['id'] == int(wm_id)]
+         store_found['License_no_dash'] = license_number
+         license_found = master_licenses[master_licenses['License_no_dash'] == license_number]
+         match_found = pd.merge(store_found, license_found, left_on = 'License_no_dash', right_on = 'License_no_dash', how = 'inner')
+         licenses_joined = licenses_joined.append(match_found, ignore_index = True, sort = False)
+
+
    def delete_join(self, license_id, wm_id):
-      with open(self.joined_location) as joined_file:
+      with open(self.joined_json_location) as joined_file:
          joined_data = json.load(joined_file)
 
       wm_ids_at_key = joined_data[license_id]
       wm_ids_at_key.remove(wm_id)
 
       joined_data[license_id] = wm_ids_at_key
+      self.joined_dict = joined_data
 
-      with open(self.joined_location, 'w') as joined_file:
+      with open(self.joined_json_location, 'w') as joined_file:
          json.dump(joined_data, joined_file)
 
 
 
-class JoinSuggestions(JoinedFile):
-   def __init__(self, licenses_location, wm_location, joined_location, suggestions_location):
-      JoinedFile.__init__(self, licenses_location, wm_location, joined_location)
+class JoinSuggestions(Joining):
+   def __init__(self, licenses_location, wm_location, joined_json_location, suggestions_location):
+      Joining.__init__(self, licenses_location, wm_location, joined_json_location)
       self.suggestions_location = suggestions_location
       self.suggestions = self.read_json(self.suggestions_location)
       self.suggestions_dict = {}
@@ -170,7 +189,6 @@ class JoinSuggestions(JoinedFile):
          return self.getLicenseTuple(item_id)
 
       else:
-         print (item_id)
          bottom_tuple = self.getWMTuples([item_id])[0]
          return bottom_tuple
 
@@ -191,14 +209,18 @@ class Windows(tk.Tk):
       self.title("Join Licenses")
       self.join_suggestions = JoinSuggestions("..//data//searchResults_clean.csv", "..//data//store.csv", "..//data//latent.json", "..//data//matches.json")
       self.show_frame(self.join_suggestions.get_last_filled())
-
+      #self.show_frame(self.join_suggestions.top_ids[len(self.join_suggestions.top_ids) - 1])
 
    def show_frame(self, top_id):
-      frame = StoreRecsPage(self.container, self, self.join_suggestions, top_id)
+      if top_id != -1:
+         frame = StoreRecsPage(self.container, self, self.join_suggestions, top_id)
+      else:
+         frame = LastPage(self.container, self, self.join_suggestions, top_id)
       frame.grid(row = 0, column = 0, sticky = "nsew")
       frame.tkraise()
 
 
+### INCLUDE EMAIL.
 class StoreRecsPage(tk.Frame):
    def __init__(self, parent, controller, JoinSuggestions, top_id):
       tk.Frame.__init__(self, parent)
@@ -208,42 +230,21 @@ class StoreRecsPage(tk.Frame):
       self.controller = controller
       self.parent = parent
       
-      label = tk.Label(self, text = "Select the most similar store to the one below. ")
-      label.pack(pady = 0, padx = 0)
+      self.label = tk.Label(self, text = "Select the most similar store to the one below. ")
+      self.label.pack(pady = 0, padx = 0)
 
       self._create_top_treeview()
-
-      self.treeBottom = ttk.Treeview(self)
-      self.treeBottom['columns'] = tuple(self.join_suggestions.wm_categories)
-      self.treeBottom['displaycolumns'] = self.join_suggestions.wm_categories[1:]
-
-      for name in self.join_suggestions.wm_categories[1:]:
-         self.treeBottom.heading(name, text = name)
-
-      index = 0
-      bottom_tuples = self.join_suggestions.get_bottom_tuples(self.bottom_ids)
-
-      for bottom_tuple in bottom_tuples:
-         self.treeBottom.insert("", index, text = str(bottom_tuple[0]), values = bottom_tuple)
-         index += 1
-
-      self.treeBottom.bind("<Double-1>", self.OnDoubleClick)
-      self.treeBottom.pack(fill = X)
-      scrollbar_horizontal_bottom = ttk.Scrollbar(self, orient='horizontal', command = self.treeBottom.xview)    
-      scrollbar_horizontal_bottom.pack(fill=X)    
-      
-      self.treeBottom.configure(xscrollcommand=scrollbar_horizontal_bottom.set)
-
+      self._create_bottom_treeview()
 
       self.progress = ttk.Progressbar(self, orient=HORIZONTAL, length = 500, value = self.progress_location(), maximum = len(self.join_suggestions.top_ids), mode = 'determinate')
       self.progress.pack(fill = 'x', padx=100, pady= 1)
 
-      button_next = ttk.Button(self, text = "Next Page", command = lambda: controller.show_frame(self.join_suggestions.traverse_list(top_id)))
-      button_next.pack(side = RIGHT)
+      self.button_next = ttk.Button(self, text = "Next Page", command = lambda: controller.show_frame(self.join_suggestions.traverse_list(top_id)))
+      self.button_next.pack(side = RIGHT)
 
 
-      button_back = ttk.Button(self, text = "Previous Page", command = lambda: controller.show_frame(self.join_suggestions.traverse_list(top_id, forward = False)))
-      button_back.pack(side = LEFT)
+      self.button_back = ttk.Button(self, text = "Previous Page", command = lambda: controller.show_frame(self.join_suggestions.traverse_list(top_id, forward = False)))
+      self.button_back.pack(side = LEFT)
 
 
    def progress_location(self):
@@ -279,12 +280,49 @@ class StoreRecsPage(tk.Frame):
 
       self.treeTop.insert("", 0, text = "", values = self.join_suggestions.get_top_tuple(self.top_id))
 
+
       for store_id in self.join_suggestions.joined_dict[self.top_id]:
-         self.treeTop.insert("", 1, text = "Selected", values = self.join_suggestions.get_top_tuple(store_id, False))
+         if store_id != -1:
+            self.treeTop.insert("", 1, text = "Selected", values = self.join_suggestions.get_top_tuple(store_id, False))
+
 
       self.treeTop.bind("<Double-1>", self.DeleteOnDoubleClick)
       self.treeTop.pack()
 
+   def _create_bottom_treeview(self):
+      self.treeBottom = ttk.Treeview(self)
+      self.treeBottom['columns'] = tuple(self.join_suggestions.wm_categories)
+      self.treeBottom['displaycolumns'] = self.join_suggestions.wm_categories[1:]
+
+      for name in self.join_suggestions.wm_categories[1:]:
+         self.treeBottom.heading(name, text = name)
+
+      index = 0
+      bottom_tuples = self.join_suggestions.get_bottom_tuples(self.bottom_ids)
+
+      for bottom_tuple in bottom_tuples:
+         self.treeBottom.insert("", index, text = str(bottom_tuple[0]), values = bottom_tuple)
+         index += 1
+
+      self.treeBottom.bind("<Double-1>", self.OnDoubleClick)
+      self.treeBottom.pack(fill = X)
+      self.scrollbar_horizontal_bottom = ttk.Scrollbar(self, orient='horizontal', command = self.treeBottom.xview)    
+      self.scrollbar_horizontal_bottom.pack(fill=X)    
+      
+      self.treeBottom.configure(xscrollcommand=self.scrollbar_horizontal_bottom.set)
+
+class LastPage(tk.Frame):
+   def __init__(self, parent, controller, JoinSuggestions, top_id):
+      tk.Frame.__init__(self, parent)
+      self.top_id = top_id
+      self.join_suggestions = JoinSuggestions
+      self.controller = controller
+      self.parent = parent
+      self.label = tk.Label(self, text = "Finished matching licenses and stores. ")
+      self.label.pack(pady = 0, padx = 0)
+
+      self.button_back = ttk.Button(self, text = "Previous Page", command = lambda: controller.show_frame(self.join_suggestions.traverse_list(self.join_suggestions.top_ids[len(self.join_suggestions.top_ids) - 1], forward = False)))
+      self.button_back.pack(side = LEFT)
       
 
 app = Windows()
